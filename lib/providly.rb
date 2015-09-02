08c0d732062b6272d6a3f98fb4fbeb9934e6efb3
Jeremy Venezia <veneziajeremy@gmail.com>
2015-09-02 12:12:58 +0200

    added users_provider_email? method

--- lib/providly.rb
+++ lib/providly.rb
@@ -1,5 +1,17 @@
 Gem.find_files('providly/**/*.rb').each { |file| require file }
 
 module Providly
-  # Your code goes here...
+  def self.uses_provider_email?(email)
+    provider_domains = read_provider_domains
+    !provider_domains.select { |domain| email.include?(domain) }.empty?
+  end
+
+  private
+
+  def self.read_provider_domains
+    provider_domains_file = File.open('lib/providly/provider_domains.txt')
+    provider_domains = []
+    provider_domains_file.each_line { |line| provider_domains << line.strip }
+    provider_domains
+  end
 end
--- spec/providly_spec.rb
+++ spec/providly_spec.rb
@@ -4,4 +4,16 @@ describe Providly do
   it 'has a version number' do
     expect(Providly::VERSION).not_to be nil
   end
+
+  describe 'uses_provider_email?' do
+    it { expect(Providly.uses_provider_email? 'email@gmail.com').to be true }
+    it { expect(Providly.uses_provider_email? 'email@elcurator.net').to be false }
+  end
+
+  describe 'read_provider_domains' do
+    subject { Providly.send(:read_provider_domains) }
+
+    it { should be_a(Array) }
+    it { should include 'gmail.com' }
+  end
 end

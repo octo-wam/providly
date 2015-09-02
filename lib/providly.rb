623b8becc54ca5b257d68d3644e2caa1899463b8
Jeremy Venezia <veneziajeremy@gmail.com>
2015-09-02 18:12:49 +0200

    fixed open file

--- lib/providly.rb
+++ lib/providly.rb
@@ -9,7 +9,8 @@ module Providly
   private
 
   def self.read_provider_domains
-    provider_domains_file = File.open('lib/providly/provider_domains.txt')
+    provider_domains_file_path = File.join(File.dirname(__FILE__), 'providly/provider_domains.txt')
+    provider_domains_file = File.open(provider_domains_file_path)
     provider_domains = []
     provider_domains_file.each_line { |line| provider_domains << line.strip }
     provider_domains

6fd248fac554da162f05979ca1fd75574f7b0094
Arnaud Doucerain <adoucerain@octo.com>
2018-10-23 12:06:01 +0200

    bsr: Use any? instead of select+empty? & Move email_domain extraction out of the block

--- lib/providly.rb
+++ lib/providly.rb
@@ -2,13 +2,13 @@ Gem.find_files('providly/**/*.rb').each { |file| require file }
 
 module Providly
   def self.email_uses_provider?(email)
-    provider_domains = read_provider_domains
-    !provider_domains.select { |domain| email.split("@").last == domain }.empty?
+    email_domain = email.split("@").last
+    provider_domains.any? { |provider_domain| email_domain == provider_domain }
   end
 
   private
 
-  def self.read_provider_domains
+  def self.provider_domains
     provider_domains_file_path = File.join(File.dirname(__FILE__), 'providly/provider_domains.txt')
     provider_domains_file = File.open(provider_domains_file_path)
     provider_domains = []
--- spec/providly_spec.rb
+++ spec/providly_spec.rb
@@ -2,22 +2,22 @@ require 'spec_helper'
 
 describe Providly do
   it 'has a version number' do
-    expect(Providly::VERSION).not_to be nil
+    expect(Providly::VERSION).not_to be_nil
   end
 
   describe '#email_uses_provider?' do
-    it { expect(Providly.email_uses_provider? 'email@gmail.com').to be true }
-    it { expect(Providly.email_uses_provider? 'email@elcurator.net').to be false }
+    it { expect(Providly.email_uses_provider? 'email@gmail.com').to eq(true) }
+    it { expect(Providly.email_uses_provider? 'email@elcurator.net').to eq(false) }
 
     it 'deals with relevant email domain including part of a provider domain (cs.com)' do
-      expect(Providly.email_uses_provider? 'email@analytics.com').to be false
+      expect(Providly.email_uses_provider? 'email@analytics.com').to eq(false)
     end
   end
 
-  describe '#read_provider_domains' do
-    subject { Providly.send(:read_provider_domains) }
+  describe '#provider_domains' do
+    subject { Providly.send(:provider_domains) }
 
-    it { should be_a(Array) }
-    it { should include 'gmail.com' }
+    it { is_expected.to be_an(Array) }
+    it { is_expected.to include('gmail.com') }
   end
 end
